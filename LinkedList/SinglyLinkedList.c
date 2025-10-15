be03b3d8e871f28dbca00247f29492a6a556f408
Charanjeet Singh Sidhu <sidhucharan53@gmail.com>
2025-10-15 10:02:18 +0530

    Added insert at mid using poistion by user

--- LinkedList/SinglyLinkedList.c
+++ LinkedList/SinglyLinkedList.c
@@ -16,6 +16,33 @@ void linearSearchinLL();
 void insertatfrontLL();
 void inseratEndLL();
 void deleteatfront();
+void insertatmidUsingPos();
+
+void insertatmidUsingPos()
+{
+    int valueinsert, pos;
+    struct Node *newnode, *temp = head;
+
+    newnode = (struct Node *)malloc(1 * (sizeof(struct Node)));
+    printf("\nInsert at Mid in Singly Linked List!!\n");
+    printf("\nEnter the position after which you want to insert in Linked List: ");
+    scanf("%d", &pos);
+    printf("\nEnter the value you want to insert at front of Linked List: ");
+    scanf("%d", &valueinsert);
+
+    newnode->Data = valueinsert;
+    newnode->Link = NULL;
+
+    int count = 0;
+    while (count < pos - 1)
+    {
+        temp = temp->Link;
+        count++;
+    }
+    newnode->Link = temp->Link;
+    temp->Link = newnode;
+    printf("Node at Pos %d has been inserted Sucessfully!!");
+}
 
 void insertatfrontLL()
 {
@@ -39,7 +66,6 @@ void insertatfrontLL()
     }
     printf("\nNode at front Value: %d inserted sucessfully!!", valueinsert);
 }
-
 void inseratEndLL()
 {
 
@@ -94,7 +120,6 @@ void linearSearchinLL()
         }
     }
 }
-
 void createLinkedList()
 {
     printf("Creation of Linked List!!\n");
@@ -117,7 +142,6 @@ void createLinkedList()
         temp = newnode;
     }
 }
-
 void traverseLinkedList()
 {
     struct Node *tempV = head;
@@ -143,10 +167,14 @@ int main()
     createLinkedList();
     createLinkedList();
     createLinkedList();
+    createLinkedList();
+    createLinkedList();
     traverseLinkedList();
-    linearSearchinLL();
-    insertatfrontLL();
+    insertatmidUsingPos();
     traverseLinkedList();
-    inseratEndLL();
-    traverseLinkedList();
+    // linearSearchinLL();
+    // insertatfrontLL();
+    // traverseLinkedList();
+    // inseratEndLL();
+    // traverseLinkedList();
 }
