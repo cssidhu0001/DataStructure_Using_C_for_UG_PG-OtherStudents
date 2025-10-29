2e61e22117dd6045839af4aae3927da1b8cc9d68
Charanjeet Singh Sidhu <sidhucharan53@gmail.com>
2025-10-29 13:02:18 +0530

    Added function to count number of odd elements present in the 2D array

--- Extra-Question/array2DMennuDriven.c
+++ Extra-Question/array2DMennuDriven.c
@@ -21,7 +21,36 @@ void create2Darray()
         }
     }
 }
+void countoddElementsof2DArray()
+{
+    printf("\nCalculating Number of Odd elements present In 2D array: ");
+    int i, j, count = 0;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (arr[i][j] % 2 != 0)
+            {
+                count++;
+            }
+        }
+    }
+    printf("\nNumber of ODD Elements present in the 2D array are : %d", count);
+}
 void addAllElementsOf2DArray()
+{
+    printf("\nCalculating  Sum of all the Elements In 2D array: ");
+    int i, j, sum = 0;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            sum += arr[i][j];
+        }
+    }
+    printf("\nSum of all the Elements  of  2D array is : %d", sum);
+}
+void addAllDiagonalsElementsOf2DArray()
 {
     printf("\nCalculating diagonal Sum When Rows and Colums are Equal In 2D array: ");
     int i, j, sum = 0;
@@ -31,11 +60,11 @@ void addAllElementsOf2DArray()
         {
             if (i == j)
             {
-                sum += i;
+                sum += arr[i][j];
             }
             if ((i + j) == i)
             {
-                sum += i;
+                sum += arr[i][j];
             }
         }
     }
@@ -61,4 +90,6 @@ int main()
     create2Darray();
     print2DArray();
     addAllElementsOf2DArray();
+    countoddElementsof2DArray();
+    addAllDiagonalsElementsOf2DArray();
 }
