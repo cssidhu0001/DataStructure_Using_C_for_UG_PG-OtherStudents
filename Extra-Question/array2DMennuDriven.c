598e849d1237acec61d8ad1b0416d30800ebb42d
Charanjeet Singh Sidhu <sidhucharan53@gmail.com>
2025-10-30 09:28:55 +0530

    Complelete 2D array Menu Driven

--- Extra-Question/array2DMennuDriven.c
+++ Extra-Question/array2DMennuDriven.c
@@ -2,6 +2,7 @@
 // elements, count the number of odd elements and find the largest among the elements stored.
 
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 100
 int arr[MAX][MAX];
 int row, col;
@@ -20,6 +21,22 @@ void create2Darray()
             scanf("%d", &arr[i][j]);
         }
     }
+}
+void findTheLargestElement()
+{
+    printf("\nFinding the Maximum Number present In 2D array: ");
+    int i, j, max = arr[0][0];
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (arr[i][j] > max)
+            {
+                max = arr[i][j];
+            }
+        }
+    }
+    printf("\nMaximum Element in 2D array is : %d", max);
 }
 void countoddElementsof2DArray()
 {
@@ -86,10 +103,45 @@ void print2DArray()
 
 int main()
 {
-    printf("2D Array Runtime Intialization: \n");
-    create2Darray();
-    print2DArray();
-    addAllElementsOf2DArray();
-    countoddElementsof2DArray();
-    addAllDiagonalsElementsOf2DArray();
+    while (1)
+    {
+        printf("\n-----MENU DRIVEN PROGRAM 2D Array-----");
+        printf("\nPRESS 1 to CREATE 2D ARRAY");
+        printf("\nPRESS 2 to PRINT 2D ARRAY");
+        printf("\nPRESS 3 PRINT THE SUM OF 2D ARRAY");
+        printf("\nPRESS 4 to PRINT THE SUM OF DIAGONALS OF 2D ARRAY");
+        printf("\nPRESS 5 to COUNT THE NUMBER OF ODD ELEMENTS PRESENT IN 2D ARRAY");
+        printf("\nPRESS 6 to FIND THE MAXIMUM  IN 2D ARRAY");
+        printf("\nPRESS 0 to EXIT");
+        printf("\n-----------------------------------------------------");
+        printf("\n\nEnter your Choice from the Above MENU");
+        int Choice;
+        scanf("%d", &Choice);
+        switch (Choice)
+        {
+        case 1:
+            create2Darray();
+            break;
+        case 2:
+            print2DArray();
+            break;
+        case 3:
+            addAllElementsOf2DArray();
+            break;
+        case 4:
+            addAllDiagonalsElementsOf2DArray();
+            break;
+        case 5:
+            countoddElementsof2DArray();
+            break;
+        case 6:
+            findTheLargestElement();
+            break;
+        case 0:
+            exit(0);
+            break;
+        default:
+            printf("INVALID CHOICE!!!");
+        }
+    }
 }
