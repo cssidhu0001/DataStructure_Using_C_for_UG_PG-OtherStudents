60ccb0312010a3081a462f80fe05a8f0af31fa5a
Charanjeet Singh Sidhu <sidhucharan53@gmail.com>
2025-11-02 19:07:35 +0530

    Added insert at End & insert at pos

--- LinkedList/SinglyLinkedUsingLocal.c
+++ LinkedList/SinglyLinkedUsingLocal.c
@@ -5,6 +5,61 @@ struct node
     int data;
     struct node *link;
 };
+void insertAtMidPosSinglyLinkedList(struct node *head, int pos)
+{
+    struct node *newnode;
+    newnode = (struct node *)malloc(1 * sizeof(struct node));
+    printf("\nEnter Value");
+    int value;
+    scanf("%d", &value);
+    newnode->data = value;
+    newnode->link = NULL;
+    struct node *temp = head;
+    int count = 0;
+    if (head == NULL)
+    {
+        head = newnode;
+        printf("\nAs Linked List is Empty !!! Node inserted in position 1!!");
+    }
+    else
+    {
+        while (count < pos - 1)
+        {
+            count++;
+            temp = temp->link;
+        }
+        newnode->link = temp;
+        temp->link = newnode;
+        printf("\nNode at Pos--> %d inserted Sucessfully!!\n", pos);
+    }
+}
+void insertAtEndSinglyLinkedList(struct node *head)
+{
+    struct node *newnode;
+    newnode = (struct node *)malloc(1 * sizeof(struct node));
+    printf("\nEnter Value");
+    int value;
+    scanf("%d", &value);
+    newnode->data = value;
+    newnode->link = NULL;
+    struct node *temp = head;
+    ;
+    if (head == NULL)
+    {
+        head = newnode;
+        printf("As Linked List is Empty !!! Node inserted in front!!");
+    }
+    else
+    {
+        while (temp->link != NULL)
+        {
+            temp = temp->link;
+        }
+        temp->link = newnode;
+        printf("\nNode at End inserted Sucessfully!!\n");
+    }
+}
+
 void insertatfrontinSinglyLinkedList(struct node **head)
 {
     struct node *newnode;
@@ -72,6 +127,12 @@ int main()
     createSinglyLinkedList(&head, &temp);
     createSinglyLinkedList(&head, &temp);
     traverseSinglyLinkedList(head);
-    insertatfrontinSinglyLinkedList(&head);
+    // insertatfrontinSinglyLinkedList(&head);
+    // insertAtEndSinglyLinkedList(head);
+    // traverseSinglyLinkedList(head);
+    printf("\nEnter the Position at which you want to Insert Value: ");
+    int insertPos;
+    scanf("%d", &insertPos);
+    insertAtMidPosSinglyLinkedList(head, insertPos);
     traverseSinglyLinkedList(head);
 }
